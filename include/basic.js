794f486c90ba29262a9e81aa4a81d86032ad005a
andreychmykh <andrey.chmykh@gmail.com>
2017-06-24 13:06:14 -0400

    Update basic.js
    Added success and error messages

--- include/basic.js
+++ include/basic.js
@@ -19,8 +19,22 @@ function submitForm() {
 		data : {
 			_subject : "Message from Website: " + $("input#name").val(),
 			email : $("input#email").val(),
-			message : $("textarea#message").val() + "\n" + $('input#phone').val()
+			message : $("textarea#message").val() + "\n"
+					+ $('input#phone').val()
 		},
-		dataType : "json"
+		dataType : "json",
+		error : onError,
+		success : onSuccess
 	});
+}
+
+function onSuccess() {
+	$("input#name").val("");
+	$("input#email").val("");
+	$("input#phone").val("");
+	$("textarea#mesage").val("");
+	alert("Thank you for the message, I'll get back to you soon!");
+}
+function onError() {
+	alert("Sorry, there was an error and your message failed to send. Feel free to contact me directly!");
 }
