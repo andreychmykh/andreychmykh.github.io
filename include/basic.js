8d54a00dc77e1e5d7239b9673689a4c62ebf77e7
andreychmykh <andrey.chmykh@gmail.com>
2017-06-24 12:22:34 -0400

    Update basic.js
    Added AJAX code to submit the form without moving the user to a new page.

--- include/basic.js
+++ include/basic.js
@@ -1,10 +1,26 @@
 $(function() {
-    $('.box').matchHeight(false);
-    $('.spotlight').matchHeight(false);
+	$('.box').matchHeight(false);
+	$('.spotlight').matchHeight(false);
 });
 
-
-$(window).resize(function () {
-	$('#top').css({'height': window.innerHeight});
-	$('.form-control').css({'min-width' : ((window.innerWidth)/2.2)});
+$(window).resize(function() {
+	$('#top').css({
+		'height' : window.innerHeight
+	});
+	$('.form-control').css({
+		'min-width' : ((window.innerWidth) / 2.2)
+	});
 });
+
+function submitForm() {
+	$.ajax({
+		url : "https://formspree.io/achmykh@andrew.cmu.edu",
+		method : "POST",
+		data : {
+			_subject : "Message from Website: " + $("input#name").val(),
+			email : $("input#email").val(),
+			message : $("textarea#message").val() + "\n" + $('input#phone').val()
+		},
+		dataType : "json"
+	});
+}
