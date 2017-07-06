8550382bb8161e5bfc8831cb047234938c860c06
andreychmykh <andrey.chmykh@gmail.com>
2017-07-06 19:35:12 -0400

    put in the color changes last
    They're the least important

--- include/complementary.js
+++ include/complementary.js
@@ -7,10 +7,14 @@ var darkest ='#222';
 
 
 function do_onload() {
+	//window resizing
+	$('#top').css({'height': window.innerHeight});
+	$('.form-control').css({'width' : ((window.innerWidth)/2.2)});
+	
 	//add the weird background thing here
 	$('body').css({'background-color': darker, 'color': lightest});
 	
-	
+	//all background colors
 	$('#about').css({'background-color': darkest, 'color': lightest});
 	$('#education').css({'background-color': light, 'color': darkest});
 	$('#experiences').css({'background-color': darkest, 'color': lightest});
@@ -19,7 +23,4 @@ function do_onload() {
 	$('#projects').css({'background-color': darkest});
 	$('#contact').css({'background-color': light, 'color': darkest});
 	$('label').css({'border-color': darkest, 'color': darkest});
-	//window resizing
-	$('#top').css({'height': window.innerHeight});
-	$('.form-control').css({'width' : ((window.innerWidth)/2.2)});
 }
